c14d4bd12775dd7b16432bcd220e4dd0ac5ee6bd
abd.maqableh <51744473+abd-maqableh@users.noreply.github.com>
2019-11-19 16:23:43 +0200

    Add the Ex

---
+++ E1.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 13:03:49 2019
+
+@author: abdul
+"""
+
+print ("Hello")
+print ("\tHello")
+print ("\t \tHello")
+
+
+print ("OrangeAcademey\n" *20 )
+
+x = float (1)
+y =float (2.8)
+z =float ("3")
+w =float ("4.2")
+print (x,y,z,w,sep ='\n')
+
+"""
+num1 =  int(input ("Enter the num"))
+num2 =  int(input ("Enter the num "))
+
+print (num1 * num2)
+"""
+"""
+num1 =  int(input ("Enter the num"))
+num2 =  int(input ("Enter the num "))
+num3 =  int(input ("Enter the num "))
+num4 =  int(input ("Enter the num "))
+num5 =  int(input ("Enter the num "))
+sumofnum = num1+num2+num3+num4+num5
+
+print (sumofnum / 5)
+
+"""
+"""
+a = 1
+b= 2.6
+c = 1j
+o = "Orange"
+A = True 
+
+print (type (a))
+print (type (b))
+print (type (c))
+print (type (o))
+print (type (A))
+"""
+
+x,y = "Orange",1
+x1,y1=100,-10
+
+print(x)
+print(y)
+print(x1)
+print (y1)
+print(" 10" * 10)
+"""
+age=input("enter your name and age")
+Age=int(age)
+div=100-Age
+print("Hello" ,  "at ", 2019+div ,"Years to turn 100")
+"""
+
+base = int(input("Enter the base of Triangle "))
+
+hight = int (input("Enter the hight of Trinagle")) 
+
+
+print (0.5 * hight * base)
+
+
+
